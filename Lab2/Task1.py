8bef3e7ac3ef71b875373bfe51f39d16324aeca6
Blitcut <edward.l.karlsson@gmail.com>
2024-04-18 19:31:14 +0200

    Fixed Lyapunov

--- Lab2/Task1.py
+++ Lab2/Task1.py
@@ -2,15 +2,18 @@ from mpl_toolkits import mplot3d
 import numpy as np
 import matplotlib.pyplot as plt
 
+# The constants for the Lorenz system
 sigma = 10
 rho = 28
 beta = 8/3
 
+# The ODEs for the Lorenz system. t is added for consistency
 dxdt = lambda x, y, z, t: sigma*(y - x)
 dydt = lambda x, y, z, t: x*(rho - z) - y
 dzdt = lambda x, y, z, t: x*y - beta*z
 
 def runge_kutta(func, coordinates, i, t, h):
+    """Applies Runge-Kutta to one coordinate for a function with 3 coordinates"""
     k1 = h * func(coordinates[0], coordinates[1], coordinates[2], t + h)
     k2 = h * func(coordinates[0] + 0.5 * h, coordinates[1] + 0.5 * h, coordinates[2] + 0.5 * h, t + 0.5*k1)
     k3 = h * func(coordinates[0] + 0.5 * h, coordinates[1] + 0.5 * h, coordinates[2] + 0.5 * h, t + 0.5 * k2)
@@ -18,33 +21,35 @@ def runge_kutta(func, coordinates, i, t, h):
 
     return coordinates[i] + (1/6)*(k1 + 2*k2 + 2*k3 + k4)
 
-def runge_kutta_3D(x, y, z, t, h):
-    coordinates = [x, y, z]
+def runge_kutta_3D(coordinates, t, h):
+    """Applies Runge-Kutta to all 3 coordinates"""
     x_new = runge_kutta(dxdt, coordinates, 0, t, h)
     y_new = runge_kutta(dydt, coordinates, 1, t, h)
     z_new = runge_kutta(dzdt, coordinates, 2, t, h)
-    return x_new, y_new, z_new
+    return [x_new, y_new, z_new]
 
 
 if __name__ == "__main__":
+    # Increments for Runge-Kutta
     h = 0.01
-    num_steps = 100000
+    # Number of steps to be taken
+    num_steps = 10000
 
+    # For ploting
     fig = plt.figure()
     ax = plt.axes(projection = '3d')
 
-    x = np.empty(num_steps + 1)
-    y = np.empty(num_steps + 1)
-    z = np.empty(num_steps + 1)
-    x[0] = 2
-    y[0] = 1
-    z[0] = 1
+    # Creates an array for the values
+    coor = np.empty((num_steps + 1, 3))
+    # Sets (x_0, y_0, z_0)
+    coor[0] = [2, 1, 1]
     t = 0
 
     for i in range(num_steps):
-        x[i + 1], y[i + 1], z[i + 1] = runge_kutta_3D(x[i], y[i], z[i], t, h)
+        # Applies Runge_Kutta for num_steps steps
+        coor[i + 1] = runge_kutta_3D(coor[i], t, h)
         t += h
         
 
-    ax.plot3D(x, y, z, 'blue')
+    ax.plot3D(coor[:, 0], coor[:, 1], coor[:, 2], 'blue')
     plt.show()
--- Lab2/Task2.py
+++ Lab2/Task2.py
@@ -1,49 +1,61 @@
 import numpy as np
+from scipy.stats import linregress
 from Task1 import *
 
+# The constants for the Lorenz system
 sigma = 10
 rho = 28
 beta = 8/3
 
+# The ODEs for the Lorenz system. t is added for consistency
 dxdt = lambda x, y, z, t: sigma*(y - x)
 dydt = lambda x, y, z, t: x*(rho - z) - y
 dzdt = lambda x, y, z, t: x*y - beta*z
 
+def find_lyapunov(normal, pertubed, h, num_steps, pert):
+    """Finds the Lyapunov exponent of for a direction"""
+    step_size = int(1/h)
+    delta_values = np.ones(int(num_steps/step_size))
+    t_values = np.arange(1, h*num_steps + 1, 1)
+
+    index = 0
+    for i in range(step_size, num_steps + 1, step_size):
+        delta_values[index] = (1/t_values[index])*np.log(np.linalg.norm(normal[i] - pertubed[i])/pert)
+        index += 1
+
+    delta  = linregress(t_values, delta_values)[0]
+    return delta
+
 if __name__ == "__main__":
-    pert = 0.0001
-    h = 0.001
-    num_steps = 1000000
+    pert = 0.00001 # The pertubation to applied
+    h = 0.001 # The step size
+    num_steps = 100000 # The number of steps
+    
 
-
-    x = np.empty(num_steps + 1)
-    y = np.empty(num_steps + 1)
-    z = np.empty(num_steps + 1)
-    x[0] = 2
-    y[0] = 1
-    z[0] = 1
-    x_pert = np.empty(num_steps + 1)
-    y_pert = np.empty(num_steps + 1)
-    z_pert = np.empty(num_steps + 1)
-    x_pert[0] = 2 + pert
-    y_pert[0] = 1 + pert
-    z_pert[0] = 1 + pert
+    # Initializes array and sets start values for the normal and pertubed cases
+    normal = np.empty((num_steps + 1, 3))
+    normal[0] = [2, 1, 1]
+    x_pert = np.empty((num_steps + 1, 3))
+    x_pert[0] = [2 + pert, 1, 1]
+    y_pert = np.empty((num_steps + 1, 3))
+    y_pert[0] = [2, 1 + pert, 1]
+    z_pert = np.empty((num_steps + 1, 3))
+    z_pert[0] = [2, 1, 1 + pert]
     t = 0
 
+    # Applies Runge-Kutta to the various pertubations
     for i in range(num_steps):
-        x[i + 1], y[i + 1], z[i + 1] = runge_kutta_3D(x[i], y[i], z[i], t, h)
-
-        x_pert[i + 1], y_pert[i + 1], z_pert[i + 1] = runge_kutta_3D(x_pert[i], y_pert[i], z_pert[i], t, h)
-
+        normal[i + 1] = runge_kutta_3D(normal[i], t, h)
+        x_pert[i + 1] = runge_kutta_3D(x_pert[i], t, h)
+        y_pert[i + 1] = runge_kutta_3D(y_pert[i], t, h)
+        z_pert[i + 1] = runge_kutta_3D(z_pert[i], t, h)
         t += h
 
-    unpertubed_start_coord = np.array([x[0], y[0], z[0]])
-    unpertubed_end_coord = np.array([x[-1], y[-1], z[-1]])
-    pertubed_start_coord = np.array([x_pert[0], y_pert[0], z_pert[0]])
-    pertubed_end_coord = np.array([x_pert[-1], y_pert[-1], z_pert[-1]])
+    # Calculates the various lyapunov exponents
+    delta_x = find_lyapunov(normal, x_pert, h, num_steps, pert)
+    delta_y = find_lyapunov(normal, y_pert, h, num_steps, pert)
+    delta_z = find_lyapunov(normal, z_pert, h, num_steps, pert)
 
-    Z0 = np.linalg.norm(unpertubed_start_coord - pertubed_start_coord)
-    Zt = np.linalg.norm(unpertubed_end_coord - pertubed_end_coord)
-
-    Lyapunov = (1/t)*np.log(Zt/Z0)
-
-    print(Lyapunov)
+    # Gets the max
+    delta_max = max([delta_x, delta_y, delta_z])
+    print("The largest Lyapunov exponent along this orbit is:", delta_max)
