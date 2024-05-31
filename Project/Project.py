2d92b44f09750680002f3902f6e7e25728b21164
ElvisSchmidt <elvis.schmidt.2002@gmail.com>
2024-05-31 15:01:45 +0200

    Project simulation done, might need some adjustment

--- Project/Project.py
+++ Project/Project.py
@@ -1,6 +1,6 @@
 import numpy as np
 import random
-
+import matplotlib.pyplot as plt
 def prisoners_dilemma(choices):
     if choices[0] == 0 and choices[1] == 0:
         return [1, 1]
@@ -16,7 +16,7 @@ def fitness_scaling(fitlist):
     minimum = np.min(fitlist)
     average = np.average(fitlist)
 
-    a = average*average/(average-minimum)
+    a = average/(average-minimum)
     b = minimum*(average)/(average-minimum)
 
     for i, fitness in enumerate(fitlist):
@@ -29,9 +29,9 @@ def crossbreed(crom1, crom2):
     child2 = np.concatenate((crom2[0:r],crom1[r:]))
     return [child1, child2]
 
-def mutation (croms):
+def mutation(croms):
     """Randomly mutates the croms"""
-    mut_rate = 0.005
+    mut_rate = 0.001
     nr_mut = int(mut_rate*croms.size)
     for i in range(nr_mut):
         x = random.randint(0, croms.shape[0] - 1)
@@ -49,6 +49,18 @@ def choose_crom(croms, probability_list):
         counter += 1
 
     return croms[counter]
+def replace(croms, newcroms, fitlist):
+    """Replaces all croms that are undesired with the new crossbreads"""
+    sortedlist = np.sort(fitlist)
+    a = sortedlist[newcroms.shape[0] - 1]
+    counter = 0
+
+    for i in range(croms.shape[0]):
+        if a >= fitlist[i]:
+            croms[i] = newcroms[counter] 
+            counter += 1
+        if counter == newcroms.shape[0]:
+            break
 
 def create_crossbreeds(croms, fitlist, newcroms):
     """Creates various crossbreads"""
@@ -63,11 +75,12 @@ def create_crossbreeds(croms, fitlist, newcroms):
     for i in range(0, newcroms.shape[0], 2):
         crom1 = choose_crom(croms, probability_list)
         crom2 = choose_crom(croms, probability_list)
-        while crom1 == crom2:
-            crom1 = choose_crom(croms, probability_list)
-            crom2 = choose_crom(croms, probability_list)
+ #       while crom1.all() == crom2.all():
+ #           crom1 = choose_crom(croms, probability_list)
+ #           crom2 = choose_crom(croms, probability_list)
 
         newcroms[i:i+2] = crossbreed(crom1, crom2)
+
 def hist_to_num(hist):
     bin_str = ''.join(f'{a}{b}' for a , b, in hist)
     bin_num = int(bin_str,2)
@@ -112,10 +125,59 @@ def pd_play(rounds, crom1 ,crom2):
             
             
 if __name__ == "__main__":
-    generations = 200
+    generations = 100
     add = 1 + 4 +  16 + 64 + 256
     crom_size = 1024+add
-    nr_of_croms = 2
-
+    nr_of_croms = 100
+    croms_culled = 10
+    avg_fitness = np.empty(generations)
+    std_dev = np.empty(generations)
+    median = np.empty(generations)
+    newcroms = np.zeros((croms_culled, crom_size))
+    
     croms = np.random.randint(2, size=(nr_of_croms, crom_size))
-    print(pd_play(20,croms[0,:],croms[1,:]))
+    for g in range(generations):
+        print(f"Generation: {g}")
+        fitlist= np.zeros([croms.shape[0]])
+        for i in range(nr_of_croms):
+            for j in range(nr_of_croms):
+                if i <= j:
+                    points = pd_play(20,croms[i,:],croms[j,:])
+                    fitlist[i] += points[0]
+                    fitlist[j] += points[1]
+        avg_fitness[g] = np.average(fitlist)
+        std_dev[g] = np.std(fitlist)
+        median[g] = np.median(fitlist)
+        if g == 99:
+            print(fitlist)
+        #fitness_scaling(fitlist)
+        create_crossbreeds(croms, fitlist, newcroms)
+        replace(croms, newcroms, fitlist)
+        mutation(croms)
+
+
+    
+
+    x = np.linspace(0, generations, generations)
+    fig, ax = plt.subplots()
+    fig, ax2 = plt.subplots()
+    fig, ax3 = plt.subplots()
+    ax.plot(x, avg_fitness)
+    ax2.plot(x, std_dev)
+    ax3.plot(x, median)
+
+    ax.set_title(f"Average result over {generations} generations")
+    ax.set_xlabel("Generations")
+    ax.set_ylabel("Average result")
+    
+    ax2.set_title(f"Average standard deviation over {generations} generations")
+    ax2.set_xlabel("Generations")
+    ax2.set_ylabel("Standard Deviation")
+
+    ax3.set_title(f"Average median over {generations} generations")
+    ax3.set_xlabel("Generations")
+    ax3.set_ylabel("Median")
+    plt.show()
+                
+                
+
